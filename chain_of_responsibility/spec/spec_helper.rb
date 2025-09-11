2bd34f92fd1fc7d32b44318b8619c3bf72a8b69c
Jarrod Folino <jarrod.folino@fundingcircle.com>
2025-09-11 16:42:59 +0100

    Added CLI test

--- chain_of_responsibility/spec/spec_helper.rb
+++ chain_of_responsibility/spec/spec_helper.rb
@@ -2,3 +2,14 @@
 
 require 'simplecov'
 SimpleCov.start
+
+require 'stringio'
+
+def capture_stdout(&blk)
+  old = $stdout
+  $stdout = fake = StringIO.new
+  blk.call
+  fake.string.split("\n")
+ensure
+  $stdout = old
+end
